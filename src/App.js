3a2d3eb71b14b9944bcd0a2b96d10c2daf827bad
ca024 <carloudpenaredondo@gmail.com>
2022-02-15 12:32:50 -0600

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello w0rtld!
+          Hello Vinci!
         </p>
         <a
           className="App-link"
